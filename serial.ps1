7d57e44240579f88ae6a99b613760fa68cfcb1ea
b-hack <148461429+b-hack@users.noreply.github.com>
2023-10-30 21:02:59 +0100

    serial.ps1

--- serial.ps1
+++ serial.ps1
@@ -4,24 +4,40 @@ $port.PortName = "COMx"  # Az Arduino soros portjának megfelelő COM portot adj
 $port.BaudRate = 9600  # A Baud sebességet az Arduino kódban beállított sebességgel egyeztetheted
 $port.Open()  # Nyitjuk a soros portot
 
-# Folyamatos olvasás a soros portról és kiírás a képernyőre
-while ($true) {
+# Az adatok fogadásához és küldéséhez használt függvények
+function Receive-Data {
     if ($port.IsOpen) {
         $receivedData = $port.ReadLine()
         Write-Host "Received data: $receivedData"
-
-        # Bemenet várakozik
-        $input = Read-Host "Enter data to send (or type 'exit' to quit)"
-
-        if ($input -eq "exit") {
-            break  # Kilépés a ciklusból, ha "exit" lett beírva
-        }
-
-        # Küldjük az inputot a soros porton
-        $port.WriteLine($input)
     } else {
-        Write-Host "Serial port is closed. Exiting..."
-        break
+        Write-Host "Serial port is closed."
+    }
+}
+
+function Send-Data($data) {
+    if ($port.IsOpen) {
+        $port.WriteLine($data)
+        Write-Host "Sent data: $data"
+    } else {
+        Write-Host "Serial port is closed."
+    }
+}
+
+# Folyamatosan figyeljük az adatokat és a bemenetet
+while ($true) {
+    # Ellenőrizzük az adatok érkezését
+    Receive-Data
+
+    # Ellenőrizzük a bemenetet
+    if ($Host.UI.RawUI.KeyAvailable) {
+        $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").Character
+        if ($key -eq "q") {
+            # Kilépünk a ciklusból, ha "q" betűt ütünk be
+            break
+        } else {
+            # Küldünk adatot, ha más betűt ütünk be
+            Send-Data $key
+        }
     }
 }
 
